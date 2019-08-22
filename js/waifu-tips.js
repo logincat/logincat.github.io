4e863438ec8c686e17ceae48333d6a5fd0523a57
logincat <38464553+logincat@users.noreply.github.com>
2019-08-22 21:55:47 +0800

    Add files via upload

---
+++ js/waifu-tips.js
@@ -0,0 +1,49 @@
+var re = /x/;
+console.log(re);
+
+/*$.ajax({
+    cache: true,
+    url: "waifu-tips.json",
+    dataType: "json",
+    success: function (result){
+        $.each(result.mouseover, function (index, tips){
+            $(document).on("mouseover", tips.selector, function (){
+                var text = tips.text;
+                if(Array.isArray(tips.text)) text = tips.text[Math.floor(Math.random() * tips.text.length + 1)-1];
+                text = text.render({text: $(this).text()});
+                showMessage(text, 3000);
+            });
+        });
+        $.each(result.click, function (index, tips){
+            $(document).on("click", tips.selector, function (){
+                var text = tips.text;
+                if(Array.isArray(tips.text)) text = tips.text[Math.floor(Math.random() * tips.text.length + 1)-1];
+                text = text.render({text: $(this).text()});
+                showMessage(text, 3000);
+            });
+        });
+    }
+});*/
+
+
+window.setInterval(showHitokoto,10000);
+
+function showHitokoto(){
+    $.getJSON('https://api.imjad.cn/hitokoto/?cat=&charset=utf-8&length=28&encode=json',function(result){
+        showMessage(result.hitokoto, 5000);
+    });
+}
+
+function showMessage(text, timeout){
+    if(Array.isArray(text)) text = text[Math.floor(Math.random() * text.length + 1)-1];
+    // console.log(text);
+    $('.waifu-tips').stop();
+    $('.waifu-tips').html(text).fadeTo(200, 1);
+    if (timeout === null) timeout = 5000;
+    hideMessage(timeout);
+}
+function hideMessage(timeout){
+    $('.waifu-tips').stop().css('opacity',1);
+    if (timeout === null) timeout = 5000;
+    $('.waifu-tips').delay(timeout).fadeTo(200, 0);
+}
